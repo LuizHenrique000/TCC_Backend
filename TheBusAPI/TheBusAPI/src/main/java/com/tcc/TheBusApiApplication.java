67c40bfa5c183dfa4522c39120960d05c6713ed4
LuizHenrique000 <luizlhra24@gmail.com>
2023-04-05 19:44:35 -0300

    Adiciona inicio do projeto

---
+++ TheBusAPI/TheBusAPI/src/main/java/com/tcc/TheBusApiApplication.java
@@ -0,0 +1,13 @@
+package com.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBusApiApplication.class, args);
+	}
+
+}
